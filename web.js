28b4eb2ac26b0ed054871320f7b6f666ca8aafda
Gabriel Bonfilio <gabrielbgomes@gmail.com>
2013-07-12 03:22:03 +0000

    Added html4

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var content = fs.readFileSync("index.html")
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html));
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
