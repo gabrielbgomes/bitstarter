a33ae3ff16c58133b100a88c4521d8bb71b966a5
Gabriel Bonfilio <gabrielbgomes@gmail.com>
2013-07-14 12:59:58 +0000

    Updates v5

--- web.js
+++ web.js
@@ -1,24 +1,12 @@
 
 var express = require('express');
-
- var fs = require('fs');
-
 var app = express.createServer(express.logger());
 
-try {
-    var data = fs.readFileSync('index.html', 'ascii');
-    response.send(data); //console.log(data);
-}
-catch (err) {
-    console.error("There was an error opening the file:");
-    response.send(err); //console.log(err);
-}
-
-
 app.get('/', function(request, response) {
-  response.send("Hello 2");
+  var fs = require('fs');
+  var content = fs.readFileSync('index.html', 'utf-8');
+  response.send(content);
 });
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
